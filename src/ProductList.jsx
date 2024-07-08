33cdcb6136a11e235899e63be1a6c37fb54679bf
Krupal Shah <krupalshah74@gmail.com>
2024-07-08 16:00:27 -0600

    Initial Commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -299,7 +299,11 @@ function ProductList() {
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-price">{plant.cost}</div>
-                                        <button onClick={() => handleAddToCart(plant)} className="product-button">Add to Cart</button>
+                                        {addedToCart[plant.name] ? (
+                                            <button className="product-button.added-to-cart" disabled>Added to Cart</button>
+                                        ) : (
+                                            <button onClick={() => handleAddToCart(plant)} className="product-button">Add to Cart</button>
+                                        )}
                                     </div>
                                 ))}
                             </div>
